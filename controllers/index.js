9aa15ee98b9ab621342e1e87d0f13988ee69b245
Shok1to <shoko.takahashi.911@gmail.com>
2023-08-08 18:40:26 -0400

    added profile handlebar.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+//const profileRoutes = require('./profileRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+//router.use('/api', profileRoutes);
 
 module.exports = router;
