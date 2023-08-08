60ead357f099d7b839cf546f91b279772b89a293
Shok1to <shoko.takahashi.911@gmail.com>
2023-08-08 12:42:23 -0400

    added comment.js

---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const newComment = async (event) => {
+    event.preventDefault();
+    
+    const postId = $('.post-title').attr('data-postId');
+    const comment = $('#comment').val().trim();
+    
+    if (!comment) {
+        alert('Nothing commented.');
+    } else {
+        const response = await fetch(`/api/comments/newComment`, {
+            method: 'POST', 
+            body: JSON.stringify({ comment, postId }), 
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace(`/api/posts/${postId}`)
+        } else {
+            alert('Unable to comment.')
+        }
+    }
+};
+
+$('.comment-form').on('submit', newComment);
